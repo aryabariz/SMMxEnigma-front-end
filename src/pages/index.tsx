0ba9b67c87b243c3a93bad24592d4df450dd813e
Arya Bariz Wicaksono <a.bariz@mail.ugm.ac.id>
2022-01-27 11:36:19 +0700

    Init

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Learn Front-end</title>
+        <meta
+          name="description"
+          content="Learning how to develop front end app"
+        />
+      </Head>
+      CLEAN INDEX
+    </div>
+  );
+};
+
+export default Home;
